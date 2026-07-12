39fd51e96b9367f53af4b3f3a34b1dd188e9195d
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2026-07-12 16:37:10 -0300

    env: remove unused error definitions

--- lapse/src/env/error.rs
+++ lapse/src/env/error.rs
@@ -4,12 +4,4 @@ use thiserror::Error;
 pub enum EnvError {
   #[error("Env {0} doesn't exist")]
   NonExistentEnv(String),
-  #[error("Could not ensure state dir: {0}")]
-  EnsureStateDir(#[source] std::io::Error),
-  #[error("Could not save state: {0}")]
-  SaveState(#[source] std::io::Error),
-  #[error("Could not open state: {0}")]
-  OpenStateFile(#[source] std::io::Error),
-  #[error("Could not read state: {0}")]
-  ReadState(#[source] std::io::Error),
 }
