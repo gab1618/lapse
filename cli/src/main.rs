b87552e5cee214b2300187a202da45a6d7a08057
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2026-07-23 18:21:31 -0300

    env: use fuzzy selector on env switch command

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -53,13 +53,15 @@ async fn entrypoint() -> error::Result<()> {
     }
     Command::Env(env_command) => {
       let lapse = Lapse::open(curr_dir)?;
+      let tree = lapse.get_resource_tree(Resource::Env, None)?;
+
       match env_command {
         cli::EnvCommand::Switch { name } => {
-          lapse.switch_env(&name)?;
-          println!("Switched to env: {}", name);
+          let seleced_env = select_tree_entry(&tree, name)?;
+          lapse.switch_env(&seleced_env)?;
+          println!("Switched to env: {}", seleced_env);
         }
         cli::EnvCommand::Ls => {
-          let tree = lapse.get_resource_tree(Resource::Env, None)?;
           // TODO: mark the current env you are in
           output_tree(0, &tree);
         }
