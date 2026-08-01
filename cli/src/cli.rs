a93becabc42552561876a2992f6dd67e1336f0e5
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2026-08-01 18:56:47 -0300

    cli: add description to commands and arguments

--- cli/src/cli.rs
+++ cli/src/cli.rs
@@ -2,41 +2,66 @@ use clap::{Parser, Subcommand};
 
 use crate::completion::CliCompletionShell;
 
-#[derive(Subcommand)]
-pub enum Command {
-  Init,
-  Ls {
-    path: Option<String>,
-  },
-  Send {
-    request: Option<String>,
-  },
-  Run {
-    script: Option<String>,
-  },
-  Completion {
-    shell: CliCompletionShell,
-  },
-  #[command(subcommand)]
-  Env(EnvCommand),
-  #[command(subcommand)]
-  Script(ScriptCommand),
-}
-
+/// Feature-rich Http CLI focused on UX
 #[derive(Parser)]
+#[command(version, long_about = None)]
 pub struct Cli {
   #[command(subcommand)]
   pub command: Command,
 }
 
 #[derive(Subcommand)]
+pub enum Command {
+  /// Initializes a Lapse space at current dir
+  Init,
+  /// Lists all requests
+  #[command(about)]
+  Ls {
+    /// Base path to list from
+    path: Option<String>,
+  },
+  /// Sends a request
+  #[command(about)]
+  Send { request: Option<String> },
+  /// Runs a script
+  #[command(about)]
+  Run { script: Option<String> },
+  /// Outputs a completion script
+  #[command(about)]
+  Completion { shell: CliCompletionShell },
+  /// Environment commands
+  #[command(subcommand, about)]
+  Env(EnvCommand),
+  /// Script commands
+  #[command(subcommand, about)]
+  Script(ScriptCommand),
+}
+
+#[derive(Subcommand)]
 pub enum EnvCommand {
-  Switch { name: Option<String> },
-  Ls { path: Option<String> },
+  #[command(about)]
+  /// Switches to another env
+  Switch {
+    /// Name of the environment
+    name: Option<String>,
+  },
+  /// Lists all environments
+  Ls {
+    /// Base path to list environments
+    path: Option<String>,
+  },
 }
 
 #[derive(Subcommand)]
 pub enum ScriptCommand {
-  Run { script: Option<String> },
-  Ls { path: Option<String> },
+  /// Runs a script
+  Run {
+    /// The script name
+    script: Option<String>,
+  },
+  /// Lists all scripts
+  Ls {
+    /// The base path to list from
+    path: Option<String>,
+  },
 }
