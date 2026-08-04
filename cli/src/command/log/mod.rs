af48f8c3409a17fe5a2e27b0fa24bfa6c4ffae51
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2026-08-04 17:07:56 -0300

    cli: add log output for when user is not using terminal

--- cli/src/command/log/mod.rs
+++ cli/src/command/log/mod.rs
@@ -1,8 +1,10 @@
 use std::{
   fmt::Write as _,
+  io,
   sync::{Arc, Mutex, MutexGuard},
 };
 
+use is_terminal::IsTerminal;
 use lapse::{log::ResponseLogsIter, tree::resource::Resource};
 
 use crate::{
@@ -39,48 +41,55 @@ pub fn log(request: Option<String>) -> crate::Result<()> {
 
   let selected_request = select_tree_entry(&tree, request)?;
 
-  let entries_iter = lapse.logs_iter(&selected_request)?;
+  if io::stdout().is_terminal() {
+    let entries_iter = lapse.logs_iter(&selected_request)?;
 
-  let shared_entries_iter = Arc::new(Mutex::new(entries_iter));
+    let shared_entries_iter = Arc::new(Mutex::new(entries_iter));
+    let pager = Pager::new();
+    pager
+      .set_run_no_overflow(true)
+      .map_err(LogError::SetupPagerConfig)?;
 
-  let pager = Pager::new();
-  pager
-    .set_run_no_overflow(true)
-    .map_err(LogError::SetupPagerConfig)?;
+    let mut thread_pager = pager.clone();
+    let thread_entries_iter = shared_entries_iter.clone();
 
-  let mut thread_pager = pager.clone();
-  let thread_entries_iter = shared_entries_iter.clone();
+    pager
+      .add_hook(
+        Hook::EofReached,
+        0,
+        Box::new(move |state| {
+          let mut entries = thread_entries_iter.lock().expect("Poisoned mutex");
+          let missing_lines = state.rows - state.screen.line_count().min(state.rows);
+          append_to_log_until_fills(&mut thread_pager, missing_lines, &mut entries)
+            .expect("Error fetching log content");
+        }),
+      )
+      .map_err(LogError::SetupPagerConfig)?;
 
-  pager
-    .add_hook(
-      Hook::EofReached,
-      0,
-      Box::new(move |state| {
-        let mut entries = thread_entries_iter.lock().expect("Poisoned mutex");
-        let missing_lines = state.rows - state.screen.line_count().min(state.rows);
-        append_to_log_until_fills(&mut thread_pager, missing_lines, &mut entries)
-          .expect("Error fetching log content");
-      }),
-    )
-    .map_err(LogError::SetupPagerConfig)?;
+    let mut thread_pager = pager.clone();
+    let thread_entries_iter = shared_entries_iter.clone();
 
-  let mut thread_pager = pager.clone();
-  let thread_entries_iter = shared_entries_iter.clone();
+    pager
+      .add_hook(
+        Hook::PrePagerStart,
+        1,
+        Box::new(move |state| {
+          let mut entries = thread_entries_iter.lock().expect("Poisoned mutex");
+          let missing_lines = state.rows - state.screen.line_count().min(state.rows);
+          append_to_log_until_fills(&mut thread_pager, missing_lines, &mut entries)
+            .expect("Error fetching log content");
+        }),
+      )
+      .unwrap();
 
-  pager
-    .add_hook(
-      Hook::PrePagerStart,
-      1,
-      Box::new(move |state| {
-        let mut entries = thread_entries_iter.lock().expect("Poisoned mutex");
-        let missing_lines = state.rows - state.screen.line_count().min(state.rows);
-        append_to_log_until_fills(&mut thread_pager, missing_lines, &mut entries)
-          .expect("Error fetching log content");
-      }),
-    )
-    .unwrap();
-
-  minus::page_all(pager).map_err(LogError::SetupPagerConfig)?;
+    minus::page_all(pager).map_err(LogError::SetupPagerConfig)?;
+  } else {
+    let entries_iter = lapse.logs_iter(&selected_request)?;
+    for (name, entry) in entries_iter {
+      println!("{name}");
+      println!("{entry}");
+    }
+  }
 
   Ok(())
 }
