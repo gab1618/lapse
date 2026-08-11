b8468839b5fe63bffce02d9482118775cbe3f301
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2026-08-11 15:29:14 -0300

    template: remove previous openapi crate
    The crate was compatible with the v2, but we intend to implement v3, as
    the example file shows

--- template/src/openapi/mod.rs
+++ template/src/openapi/mod.rs
@@ -1,9 +1,19 @@
-use openapi::Spec;
-
 use crate::Generator;
 
-impl Generator for Spec {
+pub struct OpenApiSpec {}
+
+impl Generator for OpenApiSpec {
   fn load<P: AsRef<std::path::Path>>(&self, path: P) -> crate::Result<()> {
+    let base_path = path.as_ref();
+
+    println!("{}", base_path.display());
+
     Ok(())
   }
+}
+
+#[cfg(test)]
+mod test {
+  #[test]
+  fn test_parse_example() {}
 }
