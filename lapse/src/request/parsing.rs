871797ba5bf7973cf39f4c095e4bf7492c297e17
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2026-08-19 08:16:20 -0300

    request: parse default scheme

--- lapse/src/request/parsing.rs
+++ lapse/src/request/parsing.rs
@@ -140,6 +140,8 @@ pub fn parse_request_http(doc: &str) -> crate::Result<ParsedRequest> {
   let mut request_parts = request_line.split_whitespace();
   let method = request_parts.next().ok_or(RequestError::MissingMethod)?;
   let uri = request_parts.next().ok_or(RequestError::MissingUri)?;
+  let mut url_parser = UrlParser::new(uri, "http://");
+  let parsed_uri = url_parser.parse();
 
   let mut headers = HashMap::new();
 
@@ -157,7 +159,7 @@ pub fn parse_request_http(doc: &str) -> crate::Result<ParsedRequest> {
   match method {
     "MULTIPART" => Ok(
       MultipartRequest {
-        url: uri.to_owned(),
+        url: parsed_uri,
         headers,
         body: parse_multipart_http_body(raw_body)?,
       }
@@ -165,7 +167,7 @@ pub fn parse_request_http(doc: &str) -> crate::Result<ParsedRequest> {
     ),
     "GRAPHQL" => Ok(
       GraphQLRequest {
-        url: uri.to_owned(),
+        url: parsed_uri,
         query: raw_body,
         headers,
       }
@@ -173,7 +175,7 @@ pub fn parse_request_http(doc: &str) -> crate::Result<ParsedRequest> {
     ),
     method => Ok(
       HttpRequest {
-        url: uri.to_owned(),
+        url: parsed_uri,
         method: method.to_owned(),
         headers,
         body: raw_body,
@@ -262,9 +264,66 @@ impl<'a> MultipartValueParser<'a> {
   }
 }
 
+pub struct UrlParser<'a> {
+  src: &'a str,
+  pos: usize,
+  default_scheme: &'a str,
+}
+
+impl<'a> UrlParser<'a> {
+  pub fn new(src: &'a str, default_scheme: &'a str) -> Self {
+    Self {
+      src,
+      pos: 0,
+      default_scheme,
+    }
+  }
+  fn peek_n(&self, n: usize) -> String {
+    let elements = self.src[self.pos..].chars().take(n);
+    elements.collect()
+  }
+  /// Consumes until delimiter.
+  fn consume_until_delimiter(&mut self, delimiter: &str) -> Option<String> {
+    let initial_pos = self.pos;
+
+    loop {
+      let next = self.peek_n(delimiter.len());
+      self.bump_n(1);
+
+      if next == delimiter {
+        // Found delimiter, consuming string from initial pos to current pos
+        let s: String = self.src[initial_pos..self.pos].chars().collect();
+        return Some(s);
+      }
+      if next.is_empty() {
+        break;
+      }
+    }
+
+    // Delimiter not found. Resetting position and returning None
+    self.pos = initial_pos;
+    None
+  }
+  // Returns the default scheme if none was found
+  fn consume_scheme(&mut self) -> String {
+    self
+      .consume_until_delimiter("://")
+      .unwrap_or(String::from(self.default_scheme))
+  }
+  fn bump_n(&mut self, n: usize) {
+    self.pos += n;
+  }
+  fn parse(&mut self) -> String {
+    let scheme = self.consume_scheme();
+    let remaining_chars: String = self.src[self.pos..].chars().collect();
+
+    format!("{scheme}{remaining_chars}")
+  }
+}
+
 #[cfg(test)]
 mod test {
-  use crate::request::parsing::{MultipartRequestValue, ParsedRequest};
+  use crate::request::parsing::{MultipartRequestValue, ParsedRequest, UrlParser};
 
   use super::parse_request_http;
 
@@ -293,5 +352,12 @@ mod test {
       }
       _ => panic!("It was supposed to be a multipart request"),
     }
+  }
+
+  #[test]
+  fn parses_url_without_scheme() {
+    let mut parser = UrlParser::new("localhost:3000", "http://");
+    let parsed = parser.parse();
+    assert_eq!(parsed, "http://localhost:3000");
   }
 }
