2032b25feb3b7aa7ed5f5b4d6471adf02fe0b1c5
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2026-08-20 07:51:39 -0300

    remove unused funcions

--- lapse/src/script/mod.rs
+++ lapse/src/script/mod.rs
@@ -1,56 +1,10 @@
 pub mod error;
 
-use std::{fs, ops::Deref};
-
-use mlua::{Lua, UserData, UserDataMethods};
+use std::fs;
 
 use crate::{Lapse, runner::RequestRunner, script::error::ScriptError};
 
-struct LapseLuaApi(pub Lapse);
-impl Deref for LapseLuaApi {
-  type Target = Lapse;
-
-  fn deref(&self) -> &Self::Target {
-    &self.0
-  }
-}
-
-impl UserData for LapseLuaApi {
-  fn add_methods<M: UserDataMethods<Self>>(methods: &mut M) {
-    methods.add_async_method_mut("request", |lua, this, name: String| async move {
-      let curr_env_name = this.current_env();
-      let curr_env = this.get_env(&curr_env_name).unwrap();
-      let runner = RequestRunner::new(
-        lua,
-        Default::default(),
-        curr_env.config.default_scheme.to_string(),
-      );
-
-      let req = this.get_raw_request_http(&name).unwrap();
-
-      runner
-        .execute(&req)
-        .await
-        .map_err(|_| mlua::Error::RuntimeError("Error when executing request".to_string()))
-    });
-  }
-}
-
 impl Lapse {
-  pub fn get_runtime(&self) -> crate::Result<Lua> {
-    let runtime = Lua::new();
-
-    let lapse_api = LapseLuaApi(self.clone());
-
-    let env = self.get_env(&self.current_env())?;
-
-    runtime.globals().set("env", env.variables)?;
-    runtime.globals().set("secret", env.secrets)?;
-
-    runtime.globals().set("lapse", lapse_api)?;
-
-    Ok(runtime)
-  }
   pub async fn run_script(&self, name: &str) -> crate::Result<()> {
     let script_path = self.scripts_path().join(name).with_extension("lua");
 
